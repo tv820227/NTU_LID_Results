69d5eec0493bf2e62e19f2f75106128df01f7d20
tv820227 <tv820227@gmail.com>
2018-11-14 15:25:08 +0800

    define both handles of legend and set the position  by my self

--- LID_Result/areaB_analysis_allauto.py
+++ LID_Result/areaB_analysis_allauto.py
@@ -1,4 +1,7 @@
 #%%
+# --------------------
+# pre-processing
+# --------------------
 import matplotlib.pyplot as plt
 import matplotlib.markers as mak
 import pandas as pd
@@ -6,10 +9,13 @@ import numpy as np
 import os
 
 # define to path to reach data
-path = 'D:\梁崇淵\論文\Result\OptimalResult\Billion\Picked'
+path = 'D:/梁崇淵/論文/Result/OptimalResult/Billion/Picked'
 os.chdir(path)
 
 #%%
+# --------------------
+# get data
+# --------------------
 # get data from .txt and change into DataFrame
 from glob import glob
 file_list = glob('*.txt')
@@ -31,6 +37,9 @@ for file in file_list:
     df_list.append(df)
 
 #%%
+# --------------------
+# modify DataFrame
+# --------------------
 # concat all data into one DataFrame
 alldf = pd.concat(df_list, axis='rows', ignore_index=True)
 # read information of subcatchment and merge into results of simulation
@@ -45,17 +54,18 @@ alldf['Impervious'] = alldf['Impervious']/100
 # by take mean of data
 gdf = alldf.groupby(['Sub','duration','period'], as_index=False).mean()
 groupSub = alldf.groupby('Sub', as_index=False).mean()
-#%%
-print(gdf[gdf['Sub'] == 'B12'])
 
 #%%
+# ----------------------------------------
+# plot LID distruibution 
+# ----------------------------------------
 import seaborn as sns
 # setting styles and figure
 fig ,ax= plt.subplots(figsize=(9, 7.5))
 sns.set_style("white")
-sns.set_context("paper", font_scale=1.65)
+sns.set_context("paper", font_scale=1.60)
 
-# plot box of LID ration, and point of mean and impervious ratio
+# plot box of LID ratio, and point of mean and impervious ratio
 sns.boxplot(x='Sub', y='LID_ratio',data=gdf, fliersize=0, 
 color='w',showmeans=True, 
 meanprops={'marker':'D', 'markerfacecolor':'blue', 'markersize':'8'})
@@ -65,8 +75,10 @@ size=8, label='impervious ratio', color='g')
 # size=8, label='mean of LID ratio', color='b')
 
 # only show one legend of second and third plots
-handles, labels = ax.get_legend_handles_labels()
-ax.legend(handles[0:1],labels[0:1], loc=1)
+h = [Line2D([0],[0],marker='D',color='b',markersize=6, linewidth=0),
+Line2D([0],[0],marker='o',color='g',markersize=6, linewidth=0)]
+l = ['mean of LID ratio', 'impervious ratio']
+ax.legend(h,l, loc=1)
 # ax.legend([handles[0],handles[30]],[labels[0],labels[30]], loc=1)
 
 # customizing x, y axes 
@@ -75,30 +87,79 @@ plt.ylabel('LID ratio $(\%)$')
 plt.xticks(rotation='45')
 plt.title('Distribution of LID ratio')
 plt.axis([-1,30,0,1.0])
-plt.show()
-# plt.savefig('../../New/Distribution of LID ratio.jpg')
+# plt.show()
+plt.savefig('../../New/Distribution of LID ratio.jpg')
 
-#%%
+
+# %%
+# --------------------------------------------------
+# plot the distribution under different durations
+# --------------------------------------------------
+# from matplotlib.markers import markers
+from matplotlib.lines import Line2D
 d_order = ['1hr', '2hr', '3hr', '6hr', '9hr', '12hr']
-p_order = ['2year','5year' ,'10year' ,'25year' ,'50year' ,'100year']
 # setting styles and figure
 plt.clf()
-# fig = plt.figure(figsize=(10,12))
 sns.set_style("white")
 sns.set_context("paper", font_scale=2)
 
-# plot box of LID ration, and point of mean and impervious ratio
-g = sns.factorplot(x='Sub', y='LID_ratio', kind='box',
+# plot box of LID ratio, and point of mean and impervious ratio
+g = sns.catplot(x='Sub', y='LID_ratio', kind='box',
 col='duration', col_order=d_order, col_wrap=2,
-data=gdf, fliersize=0, color='w',showmeans=True,
-meanprops={'marker':'D', 'markerfacecolor':'blue', 'markersize':'6'},
-size=5, aspect=1.25)
+data=gdf, fliersize=0, color='w',showmeans=True, meanline=False,
+meanprops={'marker':'D', 'markerfacecolor':'blue', 'markersize':'6',
+'label':'mean of LID ratio'},size=5, aspect=1.25,legend_out=False)
 g.map(sns.stripplot, x='Sub',y='Impervious',data=groupSub,
-size=6, label='impervious ratio', color='g')
+size=6, color='g', label='impervious ratio')
 
+# customizing x, y axes 
 g.set_xticklabels(rotation=90)
 g.set_xlabels('Subcatchment')
 g.set_ylabels('LID_ratio $(\%)$')
+# create handles and labels for legend
+h = [Line2D([0],[0],marker='D',color='b',markersize=6, linewidth=0),
+Line2D([0],[0],marker='o',color='g',markersize=6, linewidth=0)]
+l = ['mean', 'impervious']
+g.add_legend(handles=h, labels=l)
+
+# add legend in axes[1] (row 0 column 1) figure
+# with anchor on the upper right
+# g.axes[1].legend(handles, labels, bbox_to_anchor=(1.2,1.2,0,0))
+
 # plt.show()
 plt.savefig('../../New/Distribution of LID ratio (duration).jpg',
+bbox_inches='tight')
+
+#%%
+# --------------------------------------------------
+# plot the distribution under different periods 
+# --------------------------------------------------
+p_order = ['2year','5year' ,'10year' ,'25year' ,'50year' ,'100year']
+# setting styles and figure
+plt.clf()
+sns.set_style("white")
+sns.set_context("paper", font_scale=2)
+
+# plot box of LID ratio, and point of mean and impervious ratio
+g = sns.factorplot(x='Sub', y='LID_ratio', kind='box',
+col='period', col_order=p_order, col_wrap=2,
+data=gdf, fliersize=0, color='w',showmeans=True, meanline=False,
+meanprops={'marker':'D', 'markerfacecolor':'blue', 'markersize':'6',
+'label':'mean of LID ratio'},size=5, aspect=1.25,legend_out=True)
+g.map(sns.stripplot, x='Sub',y='Impervious',data=groupSub,
+size=6, label='impervious ratio', color='g')
+
+# customizing x, y axes 
+g.set_xticklabels(rotation=90)
+g.set_xlabels('Subcatchment')
+g.set_ylabels('LID_ratio $(\%)$')
+
+# create handles and labels for legend
+h = [Line2D([0],[0],marker='D',color='b',markersize=6, linewidth=0),
+Line2D([0],[0],marker='o',color='g',markersize=6, linewidth=0)]
+l = ['mean of LID ratio', 'impervious ratio']
+g.add_legend(handles=h, labels=l, loc='right')
+
+# plt.show()
+plt.savefig('../../New/Distribution of LID ratio (period).jpg',
 bbox_inches='tight')
